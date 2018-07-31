25b17cb1bdc04b4237eace9d4ba941d7a1f0f15e
Varazdat Manukyan <varazdat.manukyan@click2sure.co.za>
2018-07-31 17:02:26 +0400

    change app port

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express'),
       app = express(),
       home = require('./routes'),
-      users = require('./routes/users');
+      users = require('./routes/users'),
+      PORT = process.env.PORT || 3000;
 
-app.listen(3000);
+app.listen(PORT);
 
 app.use('/users', users);
 app.use('/', home);
